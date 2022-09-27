080ef6953cad63f82e4d8f234fa33e9468bdfb7a
Starlyn Fernandez <s.fernandez@innovix.com.do>
2022-09-27 12:11:29 -0400

    added component of footer
    added component of footer

---
+++ src/components/footer/Data.js
@@ -0,0 +1,14 @@
+export const ListItemData = [
+  { id: 1, title: "Home", },
+  { id: 2, title: "Cart" },
+  { id: 3, title: "Man Fashion", },
+  { id: 4, title: "Accessories" },
+  { id: 5, title: "Woman Fashion" },
+  { id: 6, title: "My Account" },
+  { id: 7, title: "Order Tracking" },
+  { id: 8, title: "Wishlist" },
+  { id: 9, title: "Wishlist" },
+  { id: 10, title: "Terms" },
+];
+
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Announcement from '../../components/Announcement/Announcemnet'
 import { Categories } from '../../components/categories/Categories'
+import { Footer } from '../../components/footer/Footer'
 import { Navbar } from '../../components/navbar/Navbar'
 import { Newsletter } from '../../components/newsLestter/Newsletter'
 
@@ -18,9 +19,8 @@ export const Home = () => {
       <Slider />
       <Categories />
       <Products />
-
-
       <Newsletter />
+      <Footer />
     </div>
   )
 }
